9151a07ba6dd58da02c740eb9727260d1923491b
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-03-19 12:21:35 +0000

    added default hero to default layout

--- perch/templates/pages/default.php
+++ perch/templates/pages/default.php
@@ -1,10 +1,20 @@
 <?php include($_SERVER['DOCUMENT_ROOT'].'/perch/runtime.php'); ?>
-<?php perch_layout('global.header'); ?>
+<?php 
+	perch_layout('global.header'); 
+	perch_content_create("Hero", array(
+		"template" => 'hero.html'
+	));
+?>
+
+<?php
+	perch_content("Hero");
+?>
 
 <div class="content">
-<?php 
-	perch_content('Page Content'); 
-?>
+	<?php 
+		perch_content('Page Content'); 
+	?>
 </div>
+
 
 <?php perch_layout('global.footer'); ?>

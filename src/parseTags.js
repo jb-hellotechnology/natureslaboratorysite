c4f9e10699a6d809db3c317fd5c04278a763df7b
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-03-22 17:11:28 +0000

    added tag display limiter and button

--- src/parseTags.js
+++ src/parseTags.js
@@ -1,32 +1,22 @@
-// const parseArrayToWords = (arr) => {
-//     let prevChar;
-//     let word = [];
-//     let wordArray = [];
-//     arr.forEach(char => {
-//         if (char > "A" && char < "z") {
-//             word = [...word, char];
-//         } else {
-//             if (prevChar > "A" && prevChar < "z") {
-//                 wordArray = [...wordArray, word.join("")];
-//                 word = [];
-//             }
-//         }
-//         prevChar = char;
-//     });
 
-//     return wordArray;
-// }
+let tagsArray = document.getElementsByClassName('post-tags');
 
-// let tagDivs = document.getElementsByClassName("post-tags");
-// console.log(tagDivs[0].innerHTML);
-// for (let i = 0; i < tagDivs.length; i++) {
-//     const tags = tagDivs[i];
-//     let tagStrArray = tags.innerHTML.split("");
-//     tags.innerHTML = null;
-//     let newTags = parseArrayToWords(tagStrArray);
-//     newTags.forEach(tag => {
-//         let element = document.createElement("DIV");
-//         element.innerHTML = tag;
-//         tags.appendChild(element);
-//     });
-// }
+for (let i = 0; i < tagsArray.length; i++) {
+    const tags = tagsArray[i];
+    if (tags.children.length > 5) {
+        for (let j = 5; j < tags.children.length; j++) {
+            const tag = tags.children[j];
+            tag.classList.add("hide");
+        }
+        let tagRevealButton = document.createElement("BUTTON");
+        tagRevealButton.innerHTML = `+ ${tags.children.length-5}`;
+        tagRevealButton.addEventListener("click", (e) => {
+            for (let j = 0; j < tags.children.length; j++) {
+                const tag = tags.children[j];
+                tag.classList.remove("hide");
+                tagRevealButton.classList.add("hide");
+            }
+        })
+        tags.appendChild(tagRevealButton);
+    }
+}

3a430ceaba7279437114834ba3feaf73cad30c96
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-04-13 11:18:53 +0100

    removed old hero section

--- perch/templates/pages/default.php
+++ perch/templates/pages/default.php
@@ -1,15 +1,7 @@
 <?php include($_SERVER['DOCUMENT_ROOT'].'/perch/runtime.php'); ?>
 <?php 
 	perch_layout('global.header'); 
-	perch_content_create("Hero", array(
-		"template" => 'hero.html'
-	));
 ?>
-
-<?php
-	perch_content("Hero");
-?>
-
 <main>
 	<?php 
 		perch_content('Page Content'); 

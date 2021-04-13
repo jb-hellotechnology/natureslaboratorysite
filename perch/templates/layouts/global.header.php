1ee960dd2197e5f0632dde186cbdfdd7524ae23a
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-04-13 12:27:44 +0100

    removed excess icon

--- perch/templates/layouts/global.header.php
+++ perch/templates/layouts/global.header.php
@@ -35,8 +35,6 @@
 <body>	
 	<?php
 		perch_content_create("Header", ["template" => "header.html"]);
-		perch_content_create("Icon", ["template" => "banner_logo.html"]);
-		perch_content("Icon");
 
 		$options = [
 			"data" => []

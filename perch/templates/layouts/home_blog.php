c6d38bc3b2c1d9bbc5099215bbe0f21d3ec41913
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-04-14 11:30:40 +0100

    updated to use responsive nav npm

--- perch/templates/layouts/home_blog.php
+++ perch/templates/layouts/home_blog.php
@@ -1,9 +1,8 @@
 <?php
-
     perch_blog_custom([
         "template" => "home_post.html",
         "filter" => "postSlug",
-        "value" => "2021-04-13-the-wonder-of-propolis"
+        "value" => perch_layout_var("slug", true)
     ])
 
 ?>

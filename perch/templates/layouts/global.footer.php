462541881ccef93832c72158f666054207ba9964
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-04-15 15:52:15 +0100

    removed unnecessary links

--- perch/templates/layouts/global.footer.php
+++ perch/templates/layouts/global.footer.php
@@ -8,5 +8,4 @@
 	<script src="/assets/js/bundle.js?v=<?php echo rand() ?>"></script>
 	<script src="/assets/js/base.js?v=<?php echo rand() ?>"></script>
 	<script src="https://kit.fontawesome.com/6b035dc64a.js" crossorigin="anonymous"></script>
-	<script src = "http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js"></script>
 	</html>

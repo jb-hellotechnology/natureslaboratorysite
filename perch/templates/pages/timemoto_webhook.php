0b451da32c9cd9e42644983304a32180e9389769
Jack Barber <jack@natureslaboratory.co.uk>
2021-05-05 16:54:18 +0100

    Update timemoto_webhook.php

--- perch/templates/pages/timemoto_webhook.php
+++ perch/templates/pages/timemoto_webhook.php
@@ -1,4 +1,24 @@
 <?php include($_SERVER['DOCUMENT_ROOT'].'/perch/runtime.php'); ?>
 <?php
 	//log timemoto event	
+	$json = file_get_contents('php://input');
+	$data = json_decode($json,true);
+	
+	if($data->event=='attendance.inserted'){
+		
+		mail('jack@jackbarber.co.uk','test',$data);
+		
+		$name = $data->data->userFullName;
+		$timeLoggedRounded = $data->data->timeLoggedRounded;
+		$attendanceStatus = $data->data->attendanceStatusId;
+		
+		if($attendanceStatus == 0){
+			$attendanceStatus = 'clocked out';
+		}elseif($attendanceStatus == 1){
+			$attendanceStatus = 'clocked in';
+		}
+		
+		timemoto_log($name,$timeLoggedRounded,$attendanceStatus,$data);
+			
+	}
 ?>

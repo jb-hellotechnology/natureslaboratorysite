2289c91b5fbba2d2ea7bf0edbe256ef7663d035e
Jack Barber <jack@natureslaboratory.co.uk>
2021-06-09 19:46:43 +0100

    QR Scanning

--- perch/addons/apps/natures_laboratory/modes/home/index.post.php
+++ perch/addons/apps/natures_laboratory/modes/home/index.post.php
@@ -1,11 +1,1 @@
 <div style="width: 500px" id="reader"></div>
-
-<script>
-	function onScanSuccess(qrCodeMessage) {
-	// handle on success condition with the decoded message
-}
-
-var html5QrcodeScanner = new Html5QrcodeScanner(
-	"reader", { fps: 10, qrbox: 250 });
-html5QrcodeScanner.render(onScanSuccess);
-</script>

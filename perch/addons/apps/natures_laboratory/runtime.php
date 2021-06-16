b3933df9d2d02258cd6511fa2a4501229be68d82
Jack Barber <jack@natureslaboratory.co.uk>
2021-06-16 16:29:08 +0100

    Update runtime.php

--- perch/addons/apps/natures_laboratory/runtime.php
+++ perch/addons/apps/natures_laboratory/runtime.php
@@ -4,7 +4,7 @@
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
 
-	require('coa/fpdf/fpdf.php');
+	require('/perch/coa/fpdf/fpdf.php');
 	
 	include('Natures_Laboratory.class.php');
 	include('Natures_Laboratorys.class.php');

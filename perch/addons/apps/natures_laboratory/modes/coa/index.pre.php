f064feb5afadefad4c4478c4e23b3ba199e7a900
Jack Barber <jack@natureslaboratory.co.uk>
2021-06-16 13:43:12 +0100

    COA Certificates

--- perch/addons/apps/natures_laboratory/modes/coa/index.pre.php
+++ perch/addons/apps/natures_laboratory/modes/coa/index.pre.php
@@ -27,6 +27,41 @@
 			    
 			}
 			
+			function Footer()
+			{
+				$this->Line(0,276,300,276);
+			    $this->SetY(-24);
+			    $this->Image('../organic.png',10,280,0,12);
+			    $this->Image('../9001.jpg',30,280,0,12);
+			    $this->SetY(-17);$this->SetX(-10);
+			    $this->SetFont('Arial','',6);
+			    $this->Cell(0,3,"Nature's Laboratory Ltd",0,1,'R');
+			    $this->SetY(-14);$this->SetX(-10);
+				$this->Cell(0,3,"Unit 3B, Enterprise Way",0,1,'R');
+				$this->SetY(-11);$this->SetX(-10);
+				$this->Cell(0,3,"Whitby, North Yorkshire",0,1,'R');
+				$this->SetY(-8);$this->SetX(-10);
+				$this->Cell(0,3,"YO22 4NH",0,1,'R');
+				
+				$this->SetY(-17);$this->SetX(45);
+				$this->Cell(60,3,"natureslaboratory.co.uk",0,0,'L');
+				$this->SetY(-14);$this->SetX(45);
+				$this->Cell(60,3,"herbalapothecaryuk.com",0,0,'L');
+				$this->SetY(-11);$this->SetX(45);
+				$this->Cell(60,3,"sweetcecilys.com",0,0,'L');
+				$this->SetY(-8);$this->SetX(45);
+				$this->Cell(60,3,"beevitalpropolis.com",0,1,'L');
+				
+				$this->SetY(-17);$this->SetX(90);
+				$this->Cell(60,3,"Vat No.: 789 4316 78",0,0,'L');
+				$this->SetY(-14);$this->SetX(90);
+				$this->Cell(60,3,"Company Reg. No.: 4375564",0,0,'L');
+				$this->SetY(-11);$this->SetX(90);
+				$this->Cell(60,3,"01947 602346",0,0,'L');
+				$this->SetY(-8);$this->SetX(90);
+				$this->Cell(60,3,"info@natureslaboratory.co.uk",0,1,'L');
+			}
+			
 			protected $B = 0;
 			protected $I = 0;
 			protected $U = 0;
@@ -111,13 +146,47 @@
 			    $this->SetStyle('U',false);
 			    $this->SetTextColor(0);
 			}
+			
+			function BasicTable($header, $data)
+			{
+			    // Colors, line width and bold font
+			    $this->SetFillColor(62,111,94);
+			    $this->SetTextColor(255);
+			    $this->SetDrawColor(0,0,0);
+			    $this->SetLineWidth(.3);
+			    $this->SetFont('','B');
+			    // Header
+			    $w = array(70, 50, 50);
+			    for($i=0;$i<count($header);$i++)
+			        $this->Cell($w[$i],7,$header[$i],1,0,'L',true);
+			    $this->Ln();
+			    // Color and font restoration
+			    $this->SetFillColor(247,247,247);
+			    $this->SetTextColor(0);
+			    $this->SetFont('');
+			    // Data
+			    $fill = false;
+			    $data2 = explode(";",$data);
+			    foreach($data2 as $row)
+			    {
+				    $row = explode(",",$row);
+			        $this->Cell($w[0],6,$row[0],'LR',0,'L',$fill);
+			        $this->Cell($w[1],6,$row[1],'LR',0,'L',$fill);
+			        $this->Cell($w[2],6,$row[2],'LR',0,'L',$fill);
+			        $this->Ln();
+			        $fill = !$fill;
+			    }
+			    // Closing line
+			    $this->Cell(array_sum($w),0,'','T');
+			}
 
 		}
 		
 		$pdf = new PDF();
 		$pdf->AddPage();
 		$pdf->Image('../nl_logo.jpg',10,10,0,20);
-		$pdf->SetFont('Arial','',8);
+		$pdf->Line(0,35,300,35);
+		$pdf->SetFont('Arial','',9);
 		$pdf->Cell(0,3,"Nature's Laboratory",0,1,'R');
 		$pdf->Cell(0,3,"Unit 3B, Enterprise Way",0,1,'R');
 		$pdf->Cell(0,3,"Whitby",0,1,'R');
@@ -128,73 +197,133 @@
 		$pdf->SetXY(10, 35);
 		$pdf->SetFont('Arial','B',16);
 		$pdf->Cell(0,10,'Certificate of Analysis: '.$specDetails['commonName'],0,1);
-		$pdf->SetFont('Arial','',8);
-		if($specDetails['productDescription']<>''){$pdf->Cell(0,5,'Product Description: '.$specDetails['productDescription'],0,1);}
-		if($specDetails['biologicalSource']<>''){$pdf->Cell(0,5,'Biological Source: '.$specDetails['biologicalSource'],0,1);}
-		if($specDetails['productDescription']<>''){$pdf->Cell(0,5,'Product Code:  '.$specDetails['productCode'],0,1);}
-		if($details['ourBatch']<>''){$pdf->Cell(0,5,'Batch Number:  '.$details['ourBatch'],0,1);}
-		if($specDetails['productDescription']<>''){$pdf->Cell(0,5,'Plant Part: '.$specDetails['plantPart'],0,1);}
-		if($details['countryOfOrigin']<>''){$pdf->Cell(0,5,'Country of Origin:  '.$details['countryOfOrigin'],0,1);}
+		$pdf->Line(0,44,300,44);
+		$pdf->SetFont('Arial','',9);
+		if($specDetails['productDescription']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Product Description: ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(100,5,$specDetails['productDescription'],0,1);}
+		if($specDetails['biologicalSource']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Biological Source: ',0,0);$pdf->SetFont('Arial','',9);$pdf->SetFont('Arial','I',9);$pdf->Cell(0,5,$specDetails['biologicalSource'],0,1);$pdf->SetFont('Arial','',9);}
+		if($specDetails['productDescription']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Product Code:  ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,$specDetails['productCode'],0,1);}
+		if($details['ourBatch']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Batch Number:  ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,$details['ourBatch'],0,1);}
+		if($specDetails['productDescription']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Plant Part: ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,$specDetails['plantPart'],0,1);}
+		if($details['countryOfOrigin']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Country of Origin:  ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,$details['countryOfOrigin'],0,1);}
 		$pdf->SetFont('Arial','B',11);
 		$pdf->Cell(0,10,'Product Description',0,1);
-		$pdf->SetFont('Arial','',8);
-		if($details['colour']<>''){$pdf->Cell(0,5,'Colour:  '.$details['colour'],0,1);}
-		if($details['odour']<>''){$pdf->Cell(0,5,'Odour:  '.$details['odour'],0,1);}
-		if($details['taste']<>''){$pdf->Cell(0,5,'Taste:  '.$details['taste'],0,1);}
+		$pdf->SetFont('Arial','',9);
+		if($details['colour']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Colour:  ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,$details['colour'],0,1);}
+		if($details['odour']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Odour:  ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,$details['odour'],0,1);}
+		if($details['taste']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Taste:  ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,$details['taste'],0,1);}
 		$pdf->SetFont('Arial','B',11);
 		$pdf->Cell(0,10,'Identification',0,1);
-		$pdf->SetFont('Arial','',8);
-		if($details['macroscopic']<>''){$pdf->Cell(0,5,'Macroscopic Characters:  '.$details['macroscopic'],0,1);}
-		if($details['microscopic']<>''){$pdf->Cell(0,5,'Microscopic Characters:  '.$details['microscopic'],0,1);}
-		$pdf->SetFont('Arial','B',11);
-		$pdf->Cell(0,10,'Tests',0,1);
-		$pdf->SetFont('Arial','',8);
-		if($details['foreignMatterAmount']<>''){$pdf->Cell(0,5,'Foreign Matter:  '.$details['foreignMatterAmount'],0,1);}
-		if($details['lossOnDryingAmount']<>''){$pdf->Cell(0,5,'Loss on Drying:  '.$details['lossOnDryingAmount'],0,1);}
-		if($details['totalAshAmount']<>''){$pdf->Cell(0,5,'Total Ash:  '.$details['totalAshAmount'],0,1);}
+		$pdf->SetFont('Arial','',9);
+		if($details['macroscopic']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Macroscopic Characters:  ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,$details['macroscopic'],0,1);}
+		if($details['microscopic']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Microscopic Characters:  ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,$details['microscopic'],0,1);}
+		
+		if($details['foreignMatterAmount']<>'' AND $details['lossOnDryingAmount']<>'' AND $details['totalAshAmount']<>''){	
+			$pdf->SetFont('Arial','B',11);
+			$pdf->Cell(0,10,'Tests',0,1);
+			$pdf->SetFont('Arial','',9);	
+			$header = array('Test','Result','Specification');
+			$data = '';
+			if($details['foreignMatterAmount']<>''){
+				$data = $data."Foreign Matter,$details[foreignMatterAmount],$specDetails[foreignMatter];";
+			}
+			if($details['lossOnDryingAmount']<>''){
+				$data = $data."Loss On Drying,$details[lossOnDryingAmount],$specDetails[lossOnDrying];";
+			}
+			if($details['totalAshAmount']<>''){
+				$data = $data."Total Ash,$details[totalAshAmount],$specDetails[totalAsh];";
+			}
+			$data = substr($data,0,-1);
+			$pdf->BasicTable($header,$data);
+			$pdf->Cell(0,3,'',0,1);
+		}
+
 		$pdf->SetFont('Arial','B',11);
 		if($details['box1']<>''){
 			$pdf->Cell(0,10,'Content',0,1);
-			$pdf->SetFont('Arial','',8);
+			$pdf->SetFont('Arial','',9);
 			$pdf->WriteHTML(nl2br($details['box1']));
 			$pdf->Cell(0,5,'',0,1);
 		}
-		$pdf->SetFont('Arial','B',11);
-		$pdf->Cell(0,10,'Toxic (Heavy) Metals',0,1);
-		$pdf->SetFont('Arial','',8);
-		if($details['leadAmount']<>''){$pdf->Cell(0,5,'Lead (Pb): '.iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$details['leadAmount']),0,1);}
-		if($details['arsenicAmount']<>''){$pdf->Cell(0,5,'Arsenic (As): '.iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$details['arsenicAmount']),0,1);}
-		if($details['mercuryAmount']<>''){$pdf->Cell(0,5,'Mercury (Hg): '.iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$details['mercuryAmount']),0,1);}
-		if($details['box1']<>''){
+	
+		if($details['leadAmount']<>'' AND $details['arsenicAmount']<>'' AND $details['mercuryAmount']<>''){
+			$pdf->SetFont('Arial','B',11);
+			$pdf->Cell(0,10,'Toxic (Heavy) Metals',0,1);
+			$pdf->SetFont('Arial','',9);
+			$header = array('Metal','Result','Specification');
+			$data = '';
+			if($details['leadAmount']<>''){
+				$data = $data."Lead (PB),".iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$details['leadAmount']).",".iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$specDetails['leadPb']).";";
+			}
+			if($details['arsenicAmount']<>''){
+				$data = $data."Arsenic (AS),".iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$details['arsenicAmount']).",".iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$specDetails['arsenicAs']).";";
+			}
+			if($details['mercuryAmount']<>''){
+				$data = $data."Mercury (Hg),".iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$details['mercuryAmount']).",".iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$specDetails['mercuryHg']).";";
+			}
+			$data = substr($data,0,-1);
+			$pdf->BasicTable($header,$data);
+			$pdf->Cell(0,3,'',0,1);
+		}
+		
+		if($details['box2']<>''){
+			$pdf->SetFont('Arial','B',9);
 			$pdf->Cell(0,5,'Additional Heavy Metals Notes:',0,1);
+			$pdf->SetFont('Arial','',9);
 			$pdf->WriteHTML(nl2br($details['box2']));
 			$pdf->Cell(0,5,'',0,1);
 		}
-		$pdf->SetFont('Arial','B',11);
-		$pdf->Cell(0,10,'Microbial Levels',0,1);
-		$pdf->SetFont('Arial','',8);
-		if($details['totalAerobicAmount']<>''){$pdf->Cell(0,5,'Total Aerobic Microbial Count: '.iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$details['totalAerobicAmount']),0,1);}
-		if($details['totalCombinedYeastMouldAmount']<>''){$pdf->Cell(0,5,'Total Combined Yeast/Moulds Count: '.iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$details['totalCombinedYeastMouldAmount']),0,1);}
-		if($details['enteroBacteriaAmount']<>''){$pdf->Cell(0,5,'Enterocateria Count (including Pseudomonas): '.iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$details['enteroBacteriaAmount']),0,1);}
-		if($details['escherichiaAmount']<>''){$pdf->Cell(0,5,'Escherichia Coli: '.iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$details['escherichiaAmount']),0,1);}
-		if($details['salmonellaAmount']<>''){$pdf->Cell(0,5,'Salmonella: '.iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$details['salmonellaAmount']),0,1);}
-		if($details['staphylococcusAmount']<>''){$pdf->Cell(0,5,'Staphylococcus Aureus: '.iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$details['staphylococcusAmount']),0,1);}
+		
+		
+		if($details['totalAerobicAmount']<>'' AND $details['totalCombinedYeastMouldAmount']<>'' AND $details['enteroBacteriaAmount']<>'' AND $details['escherichiaAmount']<>'' AND $details['salmonellaAmount']<>'' AND $details['staphylococcusAmount']<>''){
+			$pdf->SetFont('Arial','B',11);
+			$pdf->Cell(0,10,'Microbial Levels',0,1);
+			$pdf->SetFont('Arial','',9);
+			$header = array('Metal','Result','Specification');
+			$data = '';
+			if($details['totalAerobicAmount']<>''){
+				$data = $data."Total Aerobic Microbial Count,".iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$details['totalAerobicAmount']).",".iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$specDetails['totalAerobicMicrobialCount']).";";
+			}
+			if($details['totalCombinedYeastMouldAmount']<>''){
+				$data = $data."Total Combined Yeast/Moulds Count,".iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$details['totalCombinedYeastMouldAmount']).",".iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$specDetails['totalCombinedYeastMouldsCount']).";";
+			}
+			if($details['enteroBacteriaAmount']<>''){
+				$data = $data."Enterocateria Count (including Pseudomonas),".iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$details['enteroBacteriaAmount']).",".iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$specDetails['enterobacteriaCountIncludingPseudomonas']).";";	
+			}
+			if($details['escherichiaAmount']<>''){
+				$data = $data."Escherichia Coli,".iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$details['escherichiaAmount']).",".iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$specDetails['escherichiaColi']).";";
+			}
+			if($details['salmonellaAmount']<>''){
+				$data = $data."Salmonella,".iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$details['salmonellaAmount']).",".iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$specDetails['salmonella']).";";
+			}
+			if($details['staphylococcusAmount']<>''){
+				$data = $data."Staphylococcus Aureus,".iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$details['staphylococcusAmount']).",".iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$specDetails['staphylococcusAureus']).";";
+			}
+			$data = substr($data,0,-1);
+			$pdf->BasicTable($header,$data);
+		
+		}
+		
+		$pdf->Cell(0,3,'',0,1);
 		if($details['box3']<>''){
+			$pdf->SetFont('Arial','B',9);
 			$pdf->Cell(0,5,'Additional Microbial Information:',0,1);
+			$pdf->SetFont('Arial','',9);
 			$pdf->WriteHTML(nl2br($details['box3']));
 			$pdf->Cell(0,5,'',0,1);
 		}
-		if($details['mycotoxinsAmount']<>''){$pdf->Cell(0,5,'Mycotoxins (Aflatoxins, Ochratoxin A): '.iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$details['mycotoxinsAmount']),0,1);}
-		if($details['pesticidesAmount']<>''){$pdf->Cell(0,5,'Pesticides: '.iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$details['pesticidesAmount']),0,1);}
-		if($details['box1']<>''){
+		if($details['mycotoxinsAmount']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Mycotoxins (Aflatoxins, Ochratoxin A): ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$details['mycotoxinsAmount']),0,1);}
+		if($details['pesticidesAmount']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Pesticides: ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$details['pesticidesAmount']),0,1);}
+		if($details['box4']<>''){
+			$pdf->SetFont('Arial','B',9);
 			$pdf->Cell(0,5,'Additional Pesticide Notes:',0,1);
+			$pdf->SetFont('Arial','',9);
 			$pdf->WriteHTML(nl2br($details['box4']));
 			$pdf->Cell(0,5,'',0,1);
 		}
-		if($details['allergensPresent']<>''){$pdf->Cell(0,5,'Allergens: '.iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$details['allergensPresent']),0,1);}
+		if($details['allergensPresent']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Allergens: ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$details['allergensPresent']),0,1);}
 		$pdf->SetFont('Arial','B',11);
 		$pdf->Cell(0,10,'',0,1);
-		$pdf->SetFont('Arial','',8);
+		$pdf->SetFont('Arial','',9);
 		$pdf->WriteHTML("<b>Storage</b><br>Store in cool and dry condition. Keep away from direct sunlight and heat.<br><br><b>Labels</b><br>Label contains following information:<br>1. Manufacturing company name<br>2. Type of product and product code<br>3. Product Latin and common name<br>4. Product strength <br>5. Pack size<br>6. Best before date<br>7. Contact information<br><br><b>Allergen Statement</b><b>: </b>Unless otherwise stated, the products supplied are to the best of our knowledge free from nut, nut derivatives and allergens. Herbal Apothecary does handle some nut and allergen products but follows careful handling and segregation procedures. However, due to the nature of the products supplied, it is impossible for the Company to absolutely guarantee that no cross contamination has taken place at some point in the supply chain prior to delivery at our premises.<br><br><b>Non-GM Statement</b>: This product is produced or derived from ingredients supplied from non-GM sources. This is verified by our suppliers' statements and IP certificates where applicable.<br><br><b>Animal non testing statement</b>: We provide the best assurance that no animal testing is used in any phase of product development by the company, its laboratories.<br><br><b>BSE TSE Statement</b>: This is to certify that the products listed above are produced entirely from materials of natural/herbal origin and therefore are free from human or any other animal derived materials including bovine products. In addition, there are no animal derived components used in the manufacturing or handling processes of this product. As such, this material can be declared free of Bovine Spongiform Encephalopathy (BSE) and Transmissible Spongiform Encephalopathy (TSE).<br><br><b>Irradiation statement</b>: In order to address the concerns of the consumer and to ensure compliance with the legislation, Nature's Laboratory do not trade herbs have been irradiated. Purchasing specifications stipulate that irradiated herbs and spices are not acceptable, and this is checked during supplier audits at origin and processing plants.<br><br><b>Use in Production</b>: If the goods or any part thereof supplied under the contract are processed, altered or tampered with in any way by the buyer or receiver of the goods or any other person, the quality of the goods shall be deemed to be acceptable by the buyer. All customers' quality checks are to be completed on the entire load prior to production and use.<br><br><b>Additional information</b><br>1. Our product does not contain any restricted ingredients such as preservatives, additives etc.<br>2. Product consumed by general public after prescribed by herbalist or suitably qualified person.<br>3. All statements contained in this document reflect our current state of knowledge and experience, and are intended - and to be viewed - as information about this respective product only. As such, they do not constitute an exempt from any customer obligation to conduct own testing. Also, compliance with all regulations legally relevant to further processing shall be incumbent upon the customer and/or user of this product.<br><br>");
 		
 		$pdf->Output('D',"Natures Laboratory COA - $specDetails[commonName].pdf");

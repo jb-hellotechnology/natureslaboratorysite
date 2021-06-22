ac9e5aa50409fd4d05ec8f7414db53dce837c6f1
Jack Barber <jack@natureslaboratory.co.uk>
2021-06-22 11:03:09 +0100

    Update runtime.php

--- perch/addons/apps/natures_laboratory/runtime.php
+++ perch/addons/apps/natures_laboratory/runtime.php
@@ -235,7 +235,7 @@
 		$pdf->Line(0,44,300,44);
 		
 		if($image<>'https://natureslaboratory.co.uk'){
-			$pdf->Image($image,130,50,80,0);
+			$pdf->Image($image,130,50,70,0);
 		}
 		
 		$pdf->SetFont('Arial','',9);

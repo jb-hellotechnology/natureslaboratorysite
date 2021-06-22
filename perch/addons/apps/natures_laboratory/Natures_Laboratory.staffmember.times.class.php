bdf01c823f3df98096050c08ec775e10187fe966
Jack Barber <jack@natureslaboratory.co.uk>
2021-06-22 16:52:01 +0100

    Update Natures_Laboratory.staffmember.times.class.php

--- perch/addons/apps/natures_laboratory/Natures_Laboratory.staffmember.times.class.php
+++ perch/addons/apps/natures_laboratory/Natures_Laboratory.staffmember.times.class.php
@@ -167,7 +167,7 @@ class Natures_Laboratory_Staff_Member_Times extends PerchAPI_Factory
 			//echo $sql."<br />";
 			$data2 = $this->db->get_row($sql);
 			if($data2['timeType']=='clock in'){
-				$string .= ":white_check_mark: $staff[name]\n";
+				$string .= "$staff[name]\n";
 			}
 		
 		}

d9b0b451ecd7668c75ed59eabc678103daba12f1
Jack Barber <jack@natureslaboratory.co.uk>
2021-08-10 16:03:26 +0100

    COA Products Spec

---
+++ perch/addons/apps/natures_laboratory/modes/coa-products/spec/index.post.php
@@ -0,0 +1,69 @@
+ <?php
+    echo $HTML->side_panel_start();
+    
+    echo $HTML->side_panel_end();
+    
+    echo $HTML->title_panel([
+    'heading' => 'COA Products',
+    'button'  => [
+            'text' => $Lang->get('Spec'),
+            'link' => $API->app_nav().'/coa-products/spec/add',
+            'icon' => 'core/plus',
+        ],
+    ], $CurrentUser);
+
+    $Smartbar = new PerchSmartbar($CurrentUser, $HTML, $Lang);
+
+	$Smartbar->add_item([
+	    'active' => false,
+	    'title' => 'COA',
+	    'link'  => $API->app_nav().'/coa-products/',
+	]);
+	
+	$Smartbar->add_item([
+	    'active' => true,
+	    'title' => 'Spec',
+	    'link'  => $API->app_nav().'/coa-products/spec/',
+	]);
+	
+	$Smartbar->add_item([
+	    'active' => false,
+	    'title' => 'Countries',
+	    'link'  => $API->app_nav().'/coa-products/countries/',
+	]);
+	
+	
+	echo $Smartbar->render();
+
+    echo $HTML->main_panel_start(); 
+
+?>
+	<table class="d">
+        <thead>
+            <tr>
+                <th class="first">Product Code</th>
+				<th>Common Name</th>
+                <th>View/Edit</th>
+	            <th>Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+<?php
+    foreach($spec as $Spec) {
+
+?>
+            <tr>
+                <td><?php echo $Spec->productCode(); ?></td>
+                <td><?php echo $Spec->commonName(); ?></td>
+                <td><a href="<?php echo $HTML->encode($API->app_path()); ?>/coa-products/spec/edit/?id=<?php echo $HTML->encode(urlencode($Spec->natures_laboratory_coa_products_specID())); ?>" class="delete inline-delete"><?php echo 'View/Edit'; ?></a></td>
+                <td><a href="<?php echo $HTML->encode($API->app_path()); ?>/coa-products/spec/delete/?id=<?php echo $HTML->encode(urlencode($Spec->natures_laboratory_coa_products_specID())); ?>" class="delete inline-delete"><?php echo 'Delete'; ?></a></td>
+            </tr>
+<?php
+	}
+?>
+	    </tbody>
+    </table>
+
+<?php		
+
+    echo $HTML->main_panel_end();
---
+++ perch/addons/apps/natures_laboratory/modes/coa-products/spec/index.pre.php
@@ -0,0 +1,12 @@
+<?php
+	
+	if (!$CurrentUser->has_priv('natures_laboratory.coa')) exit;
+
+	$NaturesLaboratoryCOASpec = new Natures_Laboratory_COA_Products_Specs($API); 
+    
+    $HTML = $API->get('HTML');
+    $Form = $API->get('Form');
+    $Template = $API->get('Template');
+    
+    $spec = array();
+	$spec = $NaturesLaboratoryCOASpec->all();

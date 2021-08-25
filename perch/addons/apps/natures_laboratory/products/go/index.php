4f802296c8077a702f00b21d2c86f0f14f259fcd
Jack Barber <jack@natureslaboratory.co.uk>
2021-08-25 12:29:29 +0100

    Update index.php

--- perch/addons/apps/natures_laboratory/products/go/index.php
+++ perch/addons/apps/natures_laboratory/products/go/index.php
@@ -1,27 +1,6 @@
 <?php
-	
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-	error_reporting(E_ALL);
-	
-    # include the API
-    include('../../../../../core/inc/api.php');
-    
-    $API  = new PerchAPI(1.0, 'natures_laboratory');
-
-    # include your class files
-    include('../../Natures_Laboratory.class.php');
-    include('../../Natures_Laboratorys.class.php');
-    include('../../Natures_Laboratory.coa.products.class.php');
-    include('../../Natures_Laboratory.coas.products.class.php');
-
-	$NaturesLaboratoryCOA = new Natures_Laboratory_COA_Products($API);
-
 	$batchCode = $_GET['id'];
-	$COA = $NaturesLaboratoryCOA->byBatch($batchCode);
-	if($COA){
-		header("location:https://natureslaboratory.co.uk/herbal-apothecary/get-coa/?batch=".$batchCode);
-	}else{
-		echo 'NO COA';
+	if($batchCode<>''){
+		header("location:https://herbalapothecaryuk.com/download-coa-files/?batch=".$batchCode);
 	}
 ?>

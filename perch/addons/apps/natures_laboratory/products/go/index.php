a3efcf78762b33b2ab3cb21a750b80dbee97acbe
Jack Barber <jack@natureslaboratory.co.uk>
2021-08-25 12:08:13 +0100

    COA Link

--- perch/addons/apps/natures_laboratory/products/go/index.php
+++ perch/addons/apps/natures_laboratory/products/go/index.php
@@ -1,6 +1,6 @@
 <?php
 	$batchCode = $_GET['id'];
 	if($batchCode<>''){
-		header("location:https://herbalapothecaryuk.com/download-coa-files/?batch=".$batchCode);
+		header("location:https://natureslaboratory.co.uk/herbal-apothecary/get-coa/?batch=".$batchCode);
 	}
 ?>

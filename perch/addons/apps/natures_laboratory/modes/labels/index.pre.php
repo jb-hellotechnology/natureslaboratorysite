05ad9ef21b275d567d03633b6e76a80c95ea7a68
Jack Barber <jack@natureslaboratory.co.uk>
2021-09-30 13:44:10 +0100

    Update index.pre.php

--- perch/addons/apps/natures_laboratory/modes/labels/index.pre.php
+++ perch/addons/apps/natures_laboratory/modes/labels/index.pre.php
@@ -332,7 +332,7 @@
 					$pdf->MultiCell(60, 4, $productName, 0, "L");
 					$pdf->SetXY($x, $y3);
 					$pdf->SetFont('Arial','B',6);
-					$pdf->MultiCell(90,3,"$productData[notes]",0);
+					$pdf->MultiCell(70,2,"$productData[notes]",0);
 					$pdf->SetFont('Arial','B',10);
 					$pdf->SetXY($x, $y4);
 					$pdf->Cell(90,2,"Batch: $batch  BBE: $bbe  $size",0);

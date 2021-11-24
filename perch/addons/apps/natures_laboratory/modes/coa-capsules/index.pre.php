9800354891c02a90b6bb9ed3a674d72ad336609f
Jack Barber <jack@natureslaboratory.co.uk>
2021-11-24 13:54:27 +0000

    COA Capsule PDF

--- perch/addons/apps/natures_laboratory/modes/coa-capsules/index.pre.php
+++ perch/addons/apps/natures_laboratory/modes/coa-capsules/index.pre.php
@@ -12,7 +12,7 @@
     $coa = array();
     $coa = $NaturesLaboratoryCOA->getCOAs();
     
-    if($Form->submitted()){
+	if($Form->submitted()){
 	   	$postvars = array('coa');	   
     	$data = $Form->receive($postvars);   
     	$coa = $data['coa'];
@@ -159,39 +159,6 @@
 			    $this->SetLineWidth(.3);
 			    $this->SetFont('','B');
 			    // Header
-			    $w = array(70, 50, 50);
-			    for($i=0;$i<count($header);$i++)
-			        $this->Cell($w[$i],7,$header[$i],1,0,'L',true);
-			    $this->Ln();
-			    // Color and font restoration
-			    $this->SetFillColor(247,247,247);
-			    $this->SetTextColor(0);
-			    $this->SetFont('');
-			    // Data
-			    $fill = false;
-			    $data2 = explode(";",$data);
-			    foreach($data2 as $row)
-			    {
-				    $row = explode(",",$row);
-			        $this->Cell($w[0],6,$row[0],'LR',0,'L',$fill);
-			        $this->Cell($w[1],6,$row[1],'LR',0,'L',$fill);
-			        $this->Cell($w[2],6,$row[2],'LR',0,'L',$fill);
-			        $this->Ln();
-			        $fill = !$fill;
-			    }
-			    // Closing line
-			    $this->Cell(array_sum($w),0,'','T');
-			}
-			
-			function BasicTable2($header, $data)
-			{
-			    // Colors, line width and bold font
-			    $this->SetFillColor(62,111,94);
-			    $this->SetTextColor(255);
-			    $this->SetDrawColor(0,0,0);
-			    $this->SetLineWidth(.3);
-			    $this->SetFont('','B');
-			    // Header
 			    $w = array(70, 50);
 			    for($i=0;$i<count($header);$i++)
 			        $this->Cell($w[$i],7,$header[$i],1,0,'L',true);
@@ -208,6 +175,40 @@
 				    $row = explode(",",$row);
 			        $this->Cell($w[0],6,$row[0],'LR',0,'L',$fill);
 			        $this->Cell($w[1],6,$row[1],'LR',0,'L',$fill);
+			        $this->Ln();
+			        $fill = !$fill;
+			    }
+			    // Closing line
+			    $this->Cell(array_sum($w),0,'','T');
+			}
+			
+			function BasicTable2($header, $data)
+			{
+			    // Colors, line width and bold font
+			    $this->SetFillColor(62,111,94);
+			    $this->SetTextColor(255);
+			    $this->SetDrawColor(0,0,0);
+			    $this->SetLineWidth(.3);
+			    $this->SetFont('','B');
+			    // Header
+			    $w = array(40,40,40,40);
+			    for($i=0;$i<count($header);$i++)
+			        $this->Cell($w[$i],7,$header[$i],1,0,'L',true);
+			    $this->Ln();
+			    // Color and font restoration
+			    $this->SetFillColor(247,247,247);
+			    $this->SetTextColor(0);
+			    $this->SetFont('');
+			    // Data
+			    $fill = false;
+			    $data2 = explode(";",$data);
+			    foreach($data2 as $row)
+			    {
+				    $row = explode(",",$row);
+			        $this->Cell($w[0],6,$row[0],'LR',0,'L',$fill);
+			        $this->Cell($w[1],6,$row[1],'LR',0,'L',$fill);
+			        $this->Cell($w[2],6,$row[2],'LR',0,'L',$fill);
+			        $this->Cell($w[3],6,$row[3],'LR',0,'L',$fill);
 			        $this->Ln();
 			        $fill = !$fill;
 			    }
@@ -231,21 +232,25 @@
 		
 		$pdf->SetXY(10, 35);
 		$pdf->SetFont('Arial','B',16);
-		$pdf->Cell(0,10,'Certificate of Analysis: '.$specDetails['commonName'],0,1);
+		$pdf->Cell(0,10,'Certificate of Analysis: '.$details['ourBatch'],0,1);
+
+		if($details['image']['_default']<>''){
+			$pdf->Image("../../../../../perch/resources/".$details['image']['path'],140,50,0,40);
+		}
+		
+		$manDate = explode("-",$details['dateManufacture']);
+		$manDate = "$manDate[2]/$manDate[1]/$manDate[0]";
+		
+		$bbeDate = explode("-",$details['bbe']);
+		$bbeDate = "$bbeDate[2]/$bbeDate[1]/$bbeDate[0]";
+		
 		$pdf->Line(0,44,300,44);
 		$pdf->SetFont('Arial','',9);
-		if($specDetails['productType']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Type of Preparation: ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(100,5,$specDetails['productType'],0,1);}
-		if($specDetails['productCode']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Product Code: ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(100,5,$specDetails['productCode'],0,1);}
-		if($details['ourBatch']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Batch Number: ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(100,5,$details['ourBatch'],0,1);}
-		if($specDetails['biologicalSource']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Biological Source: ',0,0);$pdf->SetFont('Arial','',9);$pdf->SetFont('Arial','I',9);$pdf->Cell(0,5,$specDetails['biologicalSource'],0,1);$pdf->SetFont('Arial','',9);}
-		if($specDetails['plantPart']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Plant Part:  ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,$specDetails['plantPart'],0,1);}
-		if($specDetails['strengthVolume']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Strength Volume:  ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,$specDetails['strengthVolume'],0,1);}
-		if($specDetails['alcoholContent']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Alcohol Content:  ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,$specDetails['alcoholContent'],0,1);}
-		if($details['dateManufacture']<>NULL AND $details['dateManufacture']>'1970-01-01'){$dateManufacture = explode("-",$details['dateManufacture']);$dateManufacture = "$dateManufacture[2]/$dateManufacture[1]/$dateManufacture[0]";$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Date of Manufacturing:  ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,$dateManufacture,0,1);}
-		if($details['bbe']<>NULL AND $details['bbe']>'1970-01-01'){$bbe = explode("-",$details['bbe']);$bbe = "$bbe[2]/$bbe[1]/$bbe[0]";$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Best Before End:  ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,$bbe,0,1);}
-		$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Country of Origin:  ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,'United Kingdom',0,1);
-		if($details['countryOfOrigin']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Raw Ingredient Country of Origin:  ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,$details['countryOfOrigin'],0,1);}
-	
+		$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Product Description: ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(100,5,'Capsule',0,1);
+		$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Product Code: ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(100,5,$details['spec'],0,1);
+		$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Date of Manufacturing: ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(100,5,$manDate,0,1);
+		$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Best Before Use Date: ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(100,5,$bbeDate,0,1);
+
 		$pdf->SetFont('Arial','B',11);
 		$pdf->Cell(0,10,'Product Description',0,1);
 		$pdf->SetFont('Arial','',9);
@@ -253,37 +258,43 @@
 		if($details['odour']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Odour:  ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,$details['odour'],0,1);}
 		if($details['taste']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Taste:  ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,$details['taste'],0,1);}
 
-		if($details['pH']<>'' AND $details['gravity']<>''){	
-			$pdf->SetFont('Arial','B',11);
-			$pdf->Cell(0,10,'Tests',0,1);
-			$pdf->SetFont('Arial','',9);	
-			$header = array('Test','Result','Specification');
-			$data = '';
-			if($details['pH']<>''){
-				$data = $data."pH,$details[pH],$specDetails[pH];";
-			}
-			if($details['gravity']<>''){
-				$data = $data."Specific Gravity,$details[gravity],Not Less Than $specDetails[specificGravity];";
-			}
-			$data = substr($data,0,-1);
-			$pdf->BasicTable($header,$data);
-			$pdf->Cell(0,3,'',0,1);
+		$pdf->SetFont('Arial','B',11);
+		$pdf->Cell(0,10,'Composition',0,1);
+		$pdf->SetFont('Arial','',9);
+		$header = array('Product Code','Quantity Ratio','Botanical Source','Plant Part');
+		$data = '';
+		$components = json_decode($specDetails['natures_laboratory_coa_capsules_specDynamicFields'],true);
+		
+		foreach($components['components'] as $component){
+			$data .= "$component[productCode],$component[quantityRatio],$component[botanicalSource],$component[plantPart],";
 		}
-		
-		$thisDate = explode("-",$details['dateEntered']);
-		$thisDate = "$thisDate[2]/$thisDate[1]/$thisDate[0]";
-		
-		$pdf->WriteHTML("<br><br><br><b>Storage</b><br>Store in cool and dry condition. Keep away from direct sunlight and heat.<br><br><b>Labels</b><br>Label contains following information:<br>1. Manufacturing company name<br>2. Type of product and product code<br>3. Product Latin and common name<br>4. Pack size<br>5. Best before date<br>6. Contact information<br><br><b>Allergens</b><br><br>");
-
-		$pdf->SetFont('Arial','',9);	
-		$header = array('Allergen','Present');
-		$data = "Cereal/Wheat capsules, No;Seafood and Shellfish, No;Egg capsules, No;Fish and Fish capsules, No;Lupin (i.e. Leguminous Plants & Lupin Flour), No;Milk and Dairy capsules, No;Molluscs (including Squid & Octopus), No;Nuts and Nut capsules, No;Peanuts and capsules Thereof, No;Soybean and capsules Thereof, No;Sesame See and capsules Thereof, No;Celery and capsules Thereof, No;Mustard and capsules Thereof, No;Animal capsules, No";
+		$data = substr($data,0,-1);
 		$pdf->BasicTable2($header,$data);
 		$pdf->Cell(0,3,'',0,1);
 		
-		$pdf->WriteHTML("<br><b>Allergen Statement</b>: Unless otherwise stated, the capsules supplied are to the best of our knowledge free from nut, nut derivatives and allergens. Herbal Apothecary does handle some nut and allergen capsules but follows careful handling and segregation procedures. However, due to the nature of the capsules supplied, it is impossible for the Company to absolutely guarantee that no cross contamination has taken place at some point in the supply chain prior to delivery at our premises.<br><br><b>Non-GM Statement</b>: This product is produced or derived from ingredients supplied from non-GM sources. This is verified by our suppliers' statements and IP certificates where applicable.<br><br><b>Animal non testing statement</b>: We provide the best assurance that no animal testing is used in any phase of product development by the company, its laboratories.<br><br><b>BSE TSE Statement</b>: This is to certify that the capsules listed above are produced entirely from materials of natural/herbal origin and therefore are free from human or any other animal derived materials including bovine capsules. In addition, there are no animal derived components used in the manufacturing or handling processes of this product. As such, this material can be declared free of Bovine Spongiform Encephalopathy (BSE) and Transmissible Spongiform Encephalopathy (TSE).<br><br><b>Irradiation statement</b>: In order to address the concerns of the consumer and to ensure compliance with the legislation, Nature's Laboratory do not trade herbs have been irradiated. Purchasing specifications stipulate that irradiated herbs and spices are not acceptable, and this is checked during supplier audits at origin and processing plants.<br><br><b>Use in Production</b>: If the goods or any part thereof supplied under the contract are processed, altered or tampered with in any way by the buyer or receiver of the goods or any other person, the quality of the goods shall be deemed to be acceptable by the buyer. All customers' quality checks are to be completed on the entire load prior to production and use.<br><br><b>Additional information</b><br>1. Our product does not contain any restricted ingredients such as preservatives, additives etc.<br>2. Product consumed by general public after prescribed by herbalist or suitably qualified person.<br>3. All statements contained in this document reflect our current state of knowledge and experience, and are intended - and to be viewed - as information about this respective product only. As such, they do not constitute an exempt from any customer obligation to conduct own testing. Also, compliance with all regulations legally relevant to further processing shall be incumbent upon the customer and/or user of this product.<br><br><b>Date:</b> $thisDate<br><br>Prepared By<br><b><i>Shankar Katekhaye</i></b><br>Quality Manager");
 		
-		$pdf->Output('D',"Natures Laboratory COA - $specDetails[commonName].pdf");
+		$pdf->SetFont('Arial','B',11);
+		$pdf->Cell(0,10,'Contaminants / Impurities',0,1);
+		$pdf->SetFont('Arial','',9);
+		$pdf->WriteHTML("Testing for inorganic impurities, toxic (heavy) metals, microbial limits, mycotoxins and pesticides are carried out on a batch by batch basis according to specific customer requirements. These will normally be in line with British Pharmacopoeia limits. Nature's Laboratory Ltd do not knowingly supply material containing pesticide residues above the legal maximum residue levels.");	
+		
+		$pdf->SetFont('Arial','B',9);
+		$pdf->Cell(0,10,'',0,1);
+		
+		$thisDate = explode("-",$components['dateCreated']);
+		$thisDate = "$thisDate[2]/$thisDate[1]/$thisDate[0]";
+		
+		$pdf->WriteHTML("<b>Storage</b><br>Store in cool and dry condition. Keep away from direct sunlight and heat.<br><br><b>Labels</b><br>Label contains following information:<br>1. Manufacturing company name<br>2. Type of product and product code<br>3. Product Latin and common name<br>4. Product strength <br>5. Pack size<br>6. Best before date<br>7. Contact information<br><br><b>Allergens</b><br><br>");
+		
+		$header = array('Allergen Type','Present');
+		$data = 'Cereal/Wheat Products,No;Seafood and Shellfish,No;Egg Products,No;Fish and Fish Products,No;Lupin (i.e. Lupin Flour),No;Milk and Dairy Products,No;Molluscs (inc. Squid and Octopus),No;Nut and Nut Products,No;Peanuts and Peanut Products,No;Soybean and Soybean Products,No;Sesame Seed and Products Thereof,No;Celery and Products Thereof,No;Mustard and Products Thereof,No;Animal Products,No';
+		$pdf->BasicTable($header,$data);
+		$pdf->SetFont('Arial','B',9);
+		$pdf->Cell(0,10,'',0,1);
+		
+		$pdf->WriteHTML("<b>Allergen Statement</b><b>: </b>Unless otherwise stated, the products supplied are to the best of our knowledge free from nut, nut derivatives and allergens. Herbal Apothecary does handle some nut and allergen products but follows careful handling and segregation procedures. However, due to the nature of the products supplied, it is impossible for the Company to absolutely guarantee that no cross contamination has taken place at some point in the supply chain prior to delivery at our premises.<br><br><b>Non-GM Statement</b>: This product is produced or derived from ingredients supplied from non-GM sources. This is verified by our suppliers' statements and IP certificates where applicable.<br><br><b>Animal non testing statement</b>: We provide the best assurance that no animal testing is used in any phase of product development by the company, its laboratories.<br><br><b>BSE TSE Statement</b>: This is to certify that the products listed above are produced entirely from materials of natural/herbal origin and therefore are free from human or any other animal derived materials including bovine products. In addition, there are no animal derived components used in the manufacturing or handling processes of this product. As such, this material can be declared free of Bovine Spongiform Encephalopathy (BSE) and Transmissible Spongiform Encephalopathy (TSE).<br><br><b>Irradiation statement</b>: In order to address the concerns of the consumer and to ensure compliance with the legislation, Nature's Laboratory do not trade herbs have been irradiated. Purchasing specifications stipulate that irradiated herbs and spices are not acceptable, and this is checked during supplier audits at origin and processing plants.<br><br><b>Use in Production</b>: If the goods or any part thereof supplied under the contract are processed, altered or tampered with in any way by the buyer or receiver of the goods or any other person, the quality of the goods shall be deemed to be acceptable by the buyer. All customers' quality checks are to be completed on the entire load prior to production and use.<br><br><b>Additional information</b><br>1. Our product does not contain any restricted ingredients such as preservatives, additives etc.<br>2. Product consumed by general public after prescribed by herbalist or suitably qualified person.<br>3. All statements contained in this document reflect our current state of knowledge and experience, and are intended - and to be viewed - as information about this respective product only. As such, they do not constitute an exempt from any customer obligation to conduct own testing. Also, compliance with all regulations legally relevant to further processing shall be incumbent upon the customer and/or user of this product.<br><br><b>Date:</b> $thisDate<br><br>Prepared By<br><b><i>Shankar Katekhaye</i></b><br>Quality Manager");
+		
+		$pdf->Output('D',"Natures Laboratory COA - $details[ourBatch].pdf");
 		exit();
 		
     }
--- perch/addons/apps/natures_laboratory/modes/coa-capsules/spec/index.pre.php
+++ perch/addons/apps/natures_laboratory/modes/coa-capsules/spec/index.pre.php
@@ -18,7 +18,7 @@
     	$specData = $NaturesLaboratoryCOASpec->find($spec,true);
     	$specDetails = $specData->to_array();
     	
-    	$details = json_decode($specDetails['natures_laboratory_coa_products_specDynamicFields'],true);
+    	$details = json_decode($specDetails['natures_laboratory_coa_capsules_specDynamicFields'],true);
     	
     	class PDF extends FPDF
 		{
@@ -179,6 +179,40 @@
 			    // Closing line
 			    $this->Cell(array_sum($w),0,'','T');
 			}
+			
+			function BasicTable2($header, $data)
+			{
+			    // Colors, line width and bold font
+			    $this->SetFillColor(62,111,94);
+			    $this->SetTextColor(255);
+			    $this->SetDrawColor(0,0,0);
+			    $this->SetLineWidth(.3);
+			    $this->SetFont('','B');
+			    // Header
+			    $w = array(40,40,40,40);
+			    for($i=0;$i<count($header);$i++)
+			        $this->Cell($w[$i],7,$header[$i],1,0,'L',true);
+			    $this->Ln();
+			    // Color and font restoration
+			    $this->SetFillColor(247,247,247);
+			    $this->SetTextColor(0);
+			    $this->SetFont('');
+			    // Data
+			    $fill = false;
+			    $data2 = explode(";",$data);
+			    foreach($data2 as $row)
+			    {
+				    $row = explode(",",$row);
+			        $this->Cell($w[0],6,$row[0],'LR',0,'L',$fill);
+			        $this->Cell($w[1],6,$row[1],'LR',0,'L',$fill);
+			        $this->Cell($w[2],6,$row[2],'LR',0,'L',$fill);
+			        $this->Cell($w[3],6,$row[3],'LR',0,'L',$fill);
+			        $this->Ln();
+			        $fill = !$fill;
+			    }
+			    // Closing line
+			    $this->Cell(array_sum($w),0,'','T');
+			}
 
 		}
 		
@@ -196,7 +230,7 @@
 		
 		$pdf->SetXY(10, 35);
 		$pdf->SetFont('Arial','B',16);
-		$pdf->Cell(0,10,'Specification: '.$specDetails['commonName'],0,1);
+		$pdf->Cell(0,10,'Specification: '.$specDetails['productCode'],0,1);
 
 		if($details['image']['_default']<>''){
 			$pdf->Image("../../../../../../perch/resources/".$details['image']['path'],140,50,0,40);
@@ -204,37 +238,30 @@
 		
 		$pdf->Line(0,44,300,44);
 		$pdf->SetFont('Arial','',9);
-		if($specDetails['productDescription']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Product Description: ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(100,5,$specDetails['productDescription'],0,1);}
-		if($specDetails['productType']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Type of Preparation: ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(100,5,$specDetails['productType'],0,1);}
+		$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Product Description: ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(100,5,'Capsule',0,1);
 		if($specDetails['productCode']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Product Code: ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(100,5,$specDetails['productCode'],0,1);}
-		if($specDetails['biologicalSource']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Biological Source: ',0,0);$pdf->SetFont('Arial','',9);$pdf->SetFont('Arial','I',9);$pdf->Cell(0,5,$specDetails['biologicalSource'],0,1);$pdf->SetFont('Arial','',9);}
-		if($specDetails['plantPart']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Plant Part: ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,$specDetails['plantPart'],0,1);}
-		if($specDetails['strengthVolume']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Strength Volume: ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,$specDetails['strengthVolume'],0,1);}
-		if($specDetails['alcoholContent']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Alcohol Content: ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,$specDetails['alcoholContent'],0,1);}
-		
+
 		$pdf->SetFont('Arial','B',11);
 		$pdf->Cell(0,10,'Product Description',0,1);
 		$pdf->SetFont('Arial','',9);
 		if($specDetails['colour']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Colour:  ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,$specDetails['colour'],0,1);}
-		if($specDetails['odor']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Odour:  ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,$specDetails['odor'],0,1);}
+		if($specDetails['odour']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Odour:  ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,$specDetails['odour'],0,1);}
 		if($specDetails['taste']<>''){$pdf->SetFont('Arial','B',9);$pdf->Cell(60,5,'Taste:  ',0,0);$pdf->SetFont('Arial','',9);$pdf->Cell(0,5,$specDetails['taste'],0,1);}
+
+		$pdf->SetFont('Arial','B',11);
+		$pdf->Cell(0,10,'Composition',0,1);
+		$pdf->SetFont('Arial','',9);
+		$header = array('Product Code','Quantity Ratio','Botanical Source','Plant Part');
+		$data = '';
+		$components = json_decode($specDetails['natures_laboratory_coa_capsules_specDynamicFields'],true);
 		
-		if($specDetails['pH']<>'' OR $specDetails['specificGravity']<>''){
-			$pdf->SetFont('Arial','B',11);
-			$pdf->Cell(0,10,'pH and Specific Gravity',0,1);
-			$pdf->SetFont('Arial','',9);
-			$header = array('Test','Limits');
-			$data = '';
-			if($specDetails['pH']<>''){
-				$data = $data."pH,".iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$specDetails['pH']).";";
-			}
-			if($specDetails['specificGravity']<>''){
-				$data = $data."Specific Gravity,".iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$specDetails['specificGravity']).";";
-			}
-			$data = substr($data,0,-1);
-			$pdf->BasicTable($header,$data);
-			$pdf->Cell(0,3,'',0,1);
+		foreach($components['components'] as $component){
+			$data .= "$component[productCode],$component[quantityRatio],$component[botanicalSource],$component[plantPart],";
 		}
+		$data = substr($data,0,-1);
+		$pdf->BasicTable2($header,$data);
+		$pdf->Cell(0,3,'',0,1);
+		
 		
 		$pdf->SetFont('Arial','B',11);
 		$pdf->Cell(0,10,'Contaminants / Impurities',0,1);
@@ -244,7 +271,7 @@
 		$pdf->SetFont('Arial','B',9);
 		$pdf->Cell(0,10,'',0,1);
 		
-		$thisDate = explode("-",$details['dateCreated']);
+		$thisDate = explode("-",$specDetails['dateCreated']);
 		$thisDate = "$thisDate[2]/$thisDate[1]/$thisDate[0]";
 		
 		$pdf->WriteHTML("<b>Storage</b><br>Store in cool and dry condition. Keep away from direct sunlight and heat.<br><br><b>Labels</b><br>Label contains following information:<br>1. Manufacturing company name<br>2. Type of product and product code<br>3. Product Latin and common name<br>4. Product strength <br>5. Pack size<br>6. Best before date<br>7. Contact information<br><br><b>Allergens</b><br><br>");
@@ -257,7 +284,7 @@
 		
 		$pdf->WriteHTML("<b>Allergen Statement</b><b>: </b>Unless otherwise stated, the products supplied are to the best of our knowledge free from nut, nut derivatives and allergens. Herbal Apothecary does handle some nut and allergen products but follows careful handling and segregation procedures. However, due to the nature of the products supplied, it is impossible for the Company to absolutely guarantee that no cross contamination has taken place at some point in the supply chain prior to delivery at our premises.<br><br><b>Non-GM Statement</b>: This product is produced or derived from ingredients supplied from non-GM sources. This is verified by our suppliers' statements and IP certificates where applicable.<br><br><b>Animal non testing statement</b>: We provide the best assurance that no animal testing is used in any phase of product development by the company, its laboratories.<br><br><b>BSE TSE Statement</b>: This is to certify that the products listed above are produced entirely from materials of natural/herbal origin and therefore are free from human or any other animal derived materials including bovine products. In addition, there are no animal derived components used in the manufacturing or handling processes of this product. As such, this material can be declared free of Bovine Spongiform Encephalopathy (BSE) and Transmissible Spongiform Encephalopathy (TSE).<br><br><b>Irradiation statement</b>: In order to address the concerns of the consumer and to ensure compliance with the legislation, Nature's Laboratory do not trade herbs have been irradiated. Purchasing specifications stipulate that irradiated herbs and spices are not acceptable, and this is checked during supplier audits at origin and processing plants.<br><br><b>Use in Production</b>: If the goods or any part thereof supplied under the contract are processed, altered or tampered with in any way by the buyer or receiver of the goods or any other person, the quality of the goods shall be deemed to be acceptable by the buyer. All customers' quality checks are to be completed on the entire load prior to production and use.<br><br><b>Additional information</b><br>1. Our product does not contain any restricted ingredients such as preservatives, additives etc.<br>2. Product consumed by general public after prescribed by herbalist or suitably qualified person.<br>3. All statements contained in this document reflect our current state of knowledge and experience, and are intended - and to be viewed - as information about this respective product only. As such, they do not constitute an exempt from any customer obligation to conduct own testing. Also, compliance with all regulations legally relevant to further processing shall be incumbent upon the customer and/or user of this product.<br><br><b>Date:</b> $thisDate<br><br>Prepared By<br><b><i>Shankar Katekhaye</i></b><br>Quality Manager");
 		
-		$pdf->Output('D',"Natures Laboratory Spec - $specDetails[commonName].pdf");
+		$pdf->Output('D',"Natures Laboratory Spec - $specDetails[productCode].pdf");
 		exit();
 		
     }

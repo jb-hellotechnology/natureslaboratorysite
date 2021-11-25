bbdc6c43487a46262c326032b912380fd4acdef1
Jack Barber <jack@natureslaboratory.co.uk>
2021-11-25 15:29:38 +0000

    Update index.pre.php

--- perch/addons/apps/natures_laboratory/modes/labels/products/edit/index.pre.php
+++ perch/addons/apps/natures_laboratory/modes/labels/products/edit/index.pre.php
@@ -18,7 +18,7 @@
     if($Form->submitted()) {
     
         //FOR ITEMS PROGRAMMATICALLY ADDED TO FORM
-        $postvars = array('productCode','productName','productType','notes');	   
+        $postvars = array('productCode','productName','productType','notes', 'productRange');	   
     	$data = $Form->receive($postvars);     
 
         $new_product = $Product->update($data);

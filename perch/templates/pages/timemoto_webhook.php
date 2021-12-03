acf63e961e1b6fd24069eda39b79c62c0413513b
Jack Barber <jack@natureslaboratory.co.uk>
2021-12-03 09:39:06 +0000

    Update timemoto_webhook.php

--- perch/templates/pages/timemoto_webhook.php
+++ perch/templates/pages/timemoto_webhook.php
@@ -1,8 +1,10 @@
 <?php
+/*
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 echo 'clock';
+*/
 ?>
 <?php include($_SERVER['DOCUMENT_ROOT'].'/perch/runtime.php'); ?>
 <?php

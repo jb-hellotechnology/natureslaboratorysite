c32f6d7a4548074fb32c10fc2d7bed0f441dfc33
Jack Barber <jack@natureslaboratory.co.uk>
2023-03-17 20:06:52 +0000

    line length

--- perch/addons/apps/natures_laboratory/modes/labels/add/index.pre.php
+++ perch/addons/apps/natures_laboratory/modes/labels/add/index.pre.php
@@ -58,7 +58,7 @@
 		$name = wordwrap($productName, 22, "<br />");
 		$nameLines = explode("<br />", $name);
 		
-		$notes = wordwrap($productNotes, 60, "<br />");
+		$notes = wordwrap($productNotes, 40, "<br />");
 		$notesLines = explode("<br />", $notes);
 					
 		// Create the image

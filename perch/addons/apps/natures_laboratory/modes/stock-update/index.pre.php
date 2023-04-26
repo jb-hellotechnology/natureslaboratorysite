7fb592da0c7d7289b89071ebac87e654398958a7
Jack Barber <jack@natureslaboratory.co.uk>
2023-04-26 14:02:38 +0100

    Update index.pre.php

--- perch/addons/apps/natures_laboratory/modes/stock-update/index.pre.php
+++ perch/addons/apps/natures_laboratory/modes/stock-update/index.pre.php
@@ -20,7 +20,7 @@ error_reporting(E_ALL);
 	
 	ob_end_clean();
     
-    fputcsv($output, array("Handle", "Title", "Body (HTML)", "Vendor", "Product Category", "Type", "Tags", "Published", "Option1 Name", "Option1 Value", "Option2 Name", "Option2 Value", "Option3 Name", "Option3 Value", "Variant SKU", "Variant Grams", "Variant Inventory Tracker", "Variant Inventory Qty", "Variant Inventory Policy", "Variant Fulfillment Service", "Variant Price", "Variant Compare At Price", "Variant Requires Shipping", "Variant Taxable", "Variant Barcode", "Image Src", "Image Position", "Image Alt Text", "Gift Card", "SEO Title", "SEO Description", "Google Shopping / Google Product Category", "Google Shopping / Gender", "Google Shopping / Age Group", "Google Shopping / MPN", "Google Shopping / AdWords Grouping", "Google Shopping / AdWords Labels", "Google Shopping / Condition", "Google Shopping / Custom Product", "Google Shopping / Custom Label 0", "Google Shopping / Custom Label 1", "Google Shopping / Custom Label 2", "Google Shopping / Custom Label 3", "Google Shopping / Custom Label 4", "Variant Image", "Variant Weight Unit", "Variant Tax Code", "Cost per item", "Included / United Kingdom", "Included / European Union", "Price / European Union", "Compare At Price / European Union", "Included International", "Price / International", "Compare At Price / International", "Status"));
+    fputcsv($output, array("Handle", "Title", "Option1 Name", "Option1 Value", "Variant SKU", "Variant Inventory Qty", "Variant Price"));
     
     
     /** CHEMICALS **/
@@ -100,7 +100,7 @@ error_reporting(E_ALL);
 		
 		$taxable = "TRUE";
 		
-		$data = array($handle, $name, "", "", "", "", "", "", "Title", "Default Title", "", "", "", "", "", "", "", "$qty", "deny", "manual", "$row[SALES_PRICE]", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "active");
+		$data = array($handle, $name, "Title", "Default Title", "$qty", "$row[SALES_PRICE]");
 
 		fputcsv($output, $data);
     }
@@ -128,11 +128,11 @@ error_reporting(E_ALL);
 			
 			$SKU = str_replace("T","",$row['STOCK_CODE']);
 			
-			$data = array($handle, $name, "", "", "", "", "", "", "Title", "Default Title", "", "", "", "", "", "", "", "$qty", "continue", "manual", "$row[SALES_PRICE]", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "active");
+			$data = array($handle, $name, "Title", "Default Title", "$qty", "$row[SALES_PRICE]");
 			
 		}else{
 		
-			$data = array($handle, $name, "", "", "", "", "", "", "Title", "Default Title", "", "", "", "", "", "", "", "$qty", "deny", "manual", "$row[SALES_PRICE]", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "active");
+			$data = array($handle, $name, "Title", "Default Title", "$qty", "$row[SALES_PRICE]");
 		
 		}
 
@@ -159,7 +159,7 @@ error_reporting(E_ALL);
 		
 		$tags = str_replace("’","","$row[WEB_CATEGORY_1],$row[WEB_CATEGORY_2],$row[WEB_CATEGORY_3]");
 		
-		$data = array($handle, $name, "", "", "", "", "", "", "Title", "Default Title", "", "", "", "", "", "", "", "$qty", "deny", "manual", "$row[SALES_PRICE]", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "active");
+		$data = array($handle, $name, "Title", "Default Title", "$qty", "$row[SALES_PRICE]");
 
 		fputcsv($output, $data);
     }
@@ -202,7 +202,7 @@ error_reporting(E_ALL);
 			
 			if($row['SALES_PRICE']>0){
 			
-				$data = array($handle, $name, "", "", "", "", "", "", "Size", "$size", "", "", "", "", "", "", "", "$qty", "deny", "manual", "$row[SALES_PRICE]", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "active");
+				$data = array($handle, $name, "Size", "$size", "$qty", "$price");
 		
 				fputcsv($output, $data);
 				
@@ -228,7 +228,7 @@ error_reporting(E_ALL);
 						if($qty<1){$qty = 0;}	
 					}
 					
-					$data = array($handle, $name, "", "", "", "", "", "", "Size", "$size", "", "", "", "", "", "", "", "$qty", "deny", "manual", "$row[SALES_PRICE]", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "active");
+					$data = array($handle, $name, "Size", "$size", "$qty", "$price");
 					fputcsv($output, $data);
 			    }
 			    
@@ -253,7 +253,7 @@ error_reporting(E_ALL);
 					$qty = $organic['QTY_IN_STOCK']-$organic['QTY_ALLOCATED'];
 					if($qty<1){$qty = 0;}
 					
- 				    $data = array($handle, $name, "", "", "", "", "", "", "Size", "$size", "", "", "", "", "", "", "", "$qty", "deny", "manual", "$row[SALES_PRICE]", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "active");
+ 				    $data = array($handle, $name, "Size", "$size", "$qty", "$price");
 					fputcsv($output, $data);
 					
 					$children = $NaturesLaboratoryShopify->getOrganicChildren($organic['STOCK_CODE']);
@@ -270,7 +270,7 @@ error_reporting(E_ALL);
 						$qty = $child['QTY_IN_STOCK']-$child['QTY_ALLOCATED'];
 						if($qty<1){$qty = 0;}
 						
-					    $data = array($handle, $name, "", "", "", "", "", "", "Size", "$size", "", "", "", "", "", "", "", "$qty", "deny", "manual", "$row[SALES_PRICE]", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "active");
+					    $data = array($handle, $name, "Size", "$size", "$qty", "$price");
 						fputcsv($output, $data);
 				    }
 			    

b9f60dd721d8c29958aebd79519abfd98438bcfb
Jack Barber <jack@natureslaboratory.co.uk>
2023-11-10 16:33:40 +0000

    Update index.pre.php

--- perch/addons/apps/natures_laboratory/modes/msds/index.pre.php
+++ perch/addons/apps/natures_laboratory/modes/msds/index.pre.php
@@ -41,8 +41,8 @@
 			{
 				$this->Line(0,276,300,276);
 			    $this->SetY(-24);
-			    //$this->Image('../organic.png',10,280,0,12);
-			    //$this->Image('../9001.jpg',30,280,0,12);
+			    $this->Image('../organic.png',10,280,0,12);
+			    $this->Image('../9001.jpg',30,280,0,12);
 			    $this->SetY(-17);$this->SetX(-10);
 			    $this->SetFont('Arial','',6);
 			    $this->Cell(0,3,"Nature's Laboratory Ltd",0,1,'R');

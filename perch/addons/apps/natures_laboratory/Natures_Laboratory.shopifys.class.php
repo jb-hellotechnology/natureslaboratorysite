198df1e9d7e934669a4b44f1af9933e6e8ef9fdc
Jack Barber <jack@natureslaboratory.co.uk>
2024-01-17 13:25:48 +0000

    Update Natures_Laboratory.shopifys.class.php

--- perch/addons/apps/natures_laboratory/Natures_Laboratory.shopifys.class.php
+++ perch/addons/apps/natures_laboratory/Natures_Laboratory.shopifys.class.php
@@ -45,12 +45,14 @@ class Natures_Laboratory_Shopifys extends PerchAPI_Factory
 		    echo "Error opening file: $filename<br />";
 		}
 		
+/*
 		if($rowCount>4000){
 
 			$sql = 'SELECT * FROM perch3_natureslaboratory_stock';
 			$data = $this->db->get_rows($sql);
 			$rows = count($data);
 			if($rows>4000){
+*/
 
 				echo "More than 4000 rows in _stock<br />Truncating _stock_prev<br />";
 				
@@ -103,6 +105,7 @@ class Natures_Laboratory_Shopifys extends PerchAPI_Factory
 						$i++;
 					}
 					echo "Done!<br />";
+/*
 				}else{
 					echo '.csv file is empty #1<br />';
 				}
@@ -113,6 +116,7 @@ class Natures_Laboratory_Shopifys extends PerchAPI_Factory
 			echo '.csv file is empty #2';
 			mail('jack@natureslaboratory.co.uk','Stock Import Failed', 'HA Stock import failed');
 		}
+*/
 		
 		// UPDATE SHOPIFY STOCK LEVELS ON HA
 		$sql = "SELECT perch3_natureslaboratory_stock.STOCK_CODE AS STOCKCODE, perch3_natureslaboratory_stock.QTY_IN_STOCK AS NEWSTOCK, perch3_natureslaboratory_stock_prev.QTY_IN_STOCK AS OLDSTOCK FROM perch3_natureslaboratory_stock, perch3_natureslaboratory_stock_prev WHERE (perch3_natureslaboratory_stock.STOCK_CODE = perch3_natureslaboratory_stock_prev.STOCK_CODE) AND perch3_natureslaboratory_stock.QTY_IN_STOCK < perch3_natureslaboratory_stock_prev.QTY_IN_STOCK AND perch3_natureslaboratory_stock.STOCK_CAT=2 AND perch3_natureslaboratory_stock.WEB_PUBLISH=1 ORDER BY perch3_natureslaboratory_stock.STOCK_CODE ASC;";

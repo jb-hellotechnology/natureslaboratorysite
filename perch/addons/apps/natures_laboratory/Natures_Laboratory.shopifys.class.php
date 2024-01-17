812ff9267ee9896834e6f2248363817bc34aa231
Jack Barber <jack@natureslaboratory.co.uk>
2024-01-17 12:03:35 +0000

    Update Natures_Laboratory.shopifys.class.php

--- perch/addons/apps/natures_laboratory/Natures_Laboratory.shopifys.class.php
+++ perch/addons/apps/natures_laboratory/Natures_Laboratory.shopifys.class.php
@@ -52,7 +52,7 @@ class Natures_Laboratory_Shopifys extends PerchAPI_Factory
 			$this->db->execute($sql);
 			
 			$sql = 'SELECT * FROM perch3_natureslaboratory_stock_prev';
-$result = $this->db->query($sql);
+$result = $this->db->get_rows($sql);
 
 // Check if the query was successful
 if ($result !== false && $result instanceof mysqli_result) {

a44dea16a6d3d9e2989f3cd457f23fce9fed61ad
Jack Barber <jack@natureslaboratory.co.uk>
2024-03-15 11:52:03 +0000

    Update index.pre.php

--- perch/addons/apps/natures_laboratory/modes/goods-in/index.pre.php
+++ perch/addons/apps/natures_laboratory/modes/goods-in/index.pre.php
@@ -147,11 +147,6 @@ error_reporting(E_ALL);
 		}
 		$zip->close();
 		
-		header('Content-Type: application/zip');
-		header('Content-disposition: attachment; filename='.$zipname);
-		header('Content-Length: ' . filesize($zipname));
-		readfile($zipname);
-		
 		foreach($files as $file){ // iterate files
 		  if(is_file($file)) {
 		    unlink($file); // delete file

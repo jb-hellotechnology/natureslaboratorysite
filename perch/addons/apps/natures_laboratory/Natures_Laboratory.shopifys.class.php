4a4452d872d14e977373ccbd00b6beea985388fe
Jack Barber <jack@hellotechnology.co.uk>
2025-01-13 12:26:35 +0000

    Update Natures_Laboratory.shopifys.class.php

--- perch/addons/apps/natures_laboratory/Natures_Laboratory.shopifys.class.php
+++ perch/addons/apps/natures_laboratory/Natures_Laboratory.shopifys.class.php
@@ -354,11 +354,13 @@ class Natures_Laboratory_Shopifys extends PerchAPI_Factory
 		
 		$output = "";
 		
+/*
 		$products = "SELECT perch3_natureslaboratory_stock.STOCK_CODE AS STOCKCODE, (perch3_natureslaboratory_stock.QTY_IN_STOCK - perch3_natureslaboratory_stock.QTY_ALLOCATED) AS NEWSTOCK, (perch3_natureslaboratory_stock_prev.QTY_IN_STOCK - perch3_natureslaboratory_stock_prev.QTY_ALLOCATED) AS OLDSTOCK FROM perch3_natureslaboratory_stock, perch3_natureslaboratory_stock_prev WHERE (perch3_natureslaboratory_stock.STOCK_CODE = perch3_natureslaboratory_stock_prev.STOCK_CODE) AND ((perch3_natureslaboratory_stock.QTY_IN_STOCK != perch3_natureslaboratory_stock_prev.QTY_IN_STOCK) OR (perch3_natureslaboratory_stock.QTY_ALLOCATED != perch3_natureslaboratory_stock_prev.QTY_ALLOCATED)) AND perch3_natureslaboratory_stock.STOCK_CAT=10 AND perch3_natureslaboratory_stock.WEB_PUBLISH=1 AND RIGHT(perch3_natureslaboratory_stock.STOCK_CODE,1)<>'T' ORDER BY perch3_natureslaboratory_stock.STOCK_CODE ASC";
 		$products = $this->db->get_rows($products);
 		foreach($products as $product) {
+*/
 			
-			$productS = "SELECT * FROM perch3_natures_laboratory_shopify_bv WHERE STOCK_CODE='".$product['STOCKCODE']."'";
+			$productS = "SELECT * FROM perch3_natures_laboratory_shopify_bv WHERE RIGHT(perch3_natures_laboratory_shopify_bv.STOCK_CODE,1)<>'T' ORDER BY STOCK_CODE ASC";
 			$productS = $this->db->get_row($productS);
 			if($productS){
 				
@@ -374,7 +376,7 @@ class Natures_Laboratory_Shopifys extends PerchAPI_Factory
 				
 			}
 			
-		}
+// 		}
 		
 		if($output==''){
 			$output .= "None to update";
